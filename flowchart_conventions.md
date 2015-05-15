7b501f7f317ed0b1dfa026e514895502fa5fd0b4
Kayla <kayla.comalli@gmail.com>
2015-05-15 00:39:13 -0400

    short circuit values explained and value given.

--- flowchart_conventions.md
+++
@@ -1,3 +0,0 @@
-diamond shapes represent a point where a decision must be made and the
-code can take one of two different paths.
-
---
+++ js/short_circuit.js
@@ -0,0 +1,14 @@
+var artist = 'Rembrandt';
+var artistA = (artist || 'Unknown');
+// If the variable artist have a value, then ArtistA will
+// be given the same value as artist (because a non-empty string is truthy)
+
+var artist = '';
+var artistA = (artist || 'Unknown');
+// If the string is empty, artistA becomes a string 'Unkown'.
+
+var artist = '';
+var ArtistA = (artist || {});
+// or create an empty object if artist doesn't have a value.
+
+
